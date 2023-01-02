25a1777c469bd40e5e3bb31864ad48d0994b3907
TsaiKoga <969024089@qq.com>
2023-01-02 23:43:59 +0800

    [Add] Two prompts for the user input

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,7 +6,14 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+// 为问题增加一些要求，然后通过 Problem 指定用户的输入
+const basePromptPrefix =
+`
+Please show all the factors that cause such problems.
+
+Problem:
+`;
+
 const generateAction = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
@@ -19,7 +26,28 @@ const generateAction = async (req, res) => {
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({output: basePromptOutput});
+  // 创建第二个问题
+  const secondPrompt =
+  `
+  Take the those factors and give solutions accordingly.
+
+  Problem: ${req.body.userInput}
+
+  Factors: ${basePromptOutput.text}
+
+  Solutions:
+  `
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    temperature: 0.85,
+    max_tokens: 1250,
+  });
+
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  res.status(200).json({output: secondPromptOutput});
 }
 
 export default generateAction;
