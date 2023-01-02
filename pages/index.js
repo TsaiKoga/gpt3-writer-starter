9ef6c758c596db04207e0c9d15d02466f68ef3df
TsaiKoga <969024089@qq.com>
2023-01-02 21:52:23 +0800

    [Add] First API to OpenAI

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,33 @@ import {useState} from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [apiOutput, setApiOutput] = useState('');
+
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
   };
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("calling OpenAI...");
+    const response = await fetch('/api/generate', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const {output} = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
 
   return (
     <div className="root">
@@ -34,12 +57,25 @@ const Home = () => {
         onChange={onUserChangedText}
         />;
         <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
+          <a className="generate-button" onClick={callGenerateEndpoint}>
             <div className="generate">
               <p>Generate</p>
             </div>
           </a>
         </div>
+
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
 
       <div className="badge-container grow">
